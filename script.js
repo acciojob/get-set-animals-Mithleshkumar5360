2516796d9abd648b45895a8fd1d6a8b305894282
Mithleshkumar5360 <117704988+Mithleshkumar5360@users.noreply.github.com>
2023-04-14 19:23:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,29 @@
 //complete this code
-class Animal {}
+class Animal {
+	constructor(species){
+		this.species = species;
+	}
+	get Species(){
+		return this.species;
+	}
+	makeSound(){
+		console.log("The "+ this.species + " makes a sound.");
+	}
+}
 
-class Dog extends Animal {}
 
-class Cat extends Animal {}
+
+class Cat extends Animal {
+	purr(){
+		console.log("The "+ this.species + " purrs.");
+	}
+}
+
+class Dog extends Animal {
+	bark(){
+		console.log("The "+this.species + " barks.");
+	}
+}
 
 // Do not change the code below this line
 window.Animal = Animal;
